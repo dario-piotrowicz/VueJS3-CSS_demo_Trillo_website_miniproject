683b81247fe39c125faa2a18fcb5c5a87a78fca6
Dario Piotrowicz <dario.piotrowicz@gmail.com>
2021-02-04 20:16:27 +0000

    added basic css setup

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,30 @@ export default {
 </script>
 
 <style lang="scss">
+html {
+  box-sizing: border-box;
+
+  /*
+    We set the font-size to 62.5%, so that we generally get 1rem === 10px
+    which can help us reason around our measures
+
+    We came up with 62.5% because, browsers by default have their font-size equal to 16px,
+    and 10 is 62.5 percent of 16 ( 10/16 * 100 = 62.5 )
+  */
+  font-size: 62.5%;
+}
+
+* {
+  margin: 0;
+  padding: 0;
+}
+
+*,
+*::before,
+*::after {
+  box-sizing: inherit;
+}
+
 #app {
   h1 {
     color: green;
