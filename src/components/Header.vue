622d0bd56b2629fa83eec80c615e6a4aa34b0da5
Dario Piotrowicz <dario.piotrowicz@gmail.com>
2021-02-10 21:11:51 +0000

    added searchbar to the header component

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,12 @@
 <template>
   <header class="header">
     <img src="../assets/img/logo.png" alt="trillo logo" class="logo" />
+    <form action="#" class="search">
+      <input type="text" class="search__input" placeholder="Search hotels" />
+      <button class="search__button">
+        search
+      </button>
+    </form>
   </header>
 </template>
 
