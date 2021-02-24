b228c4db0bf473e3f5e9b8c42ac242dae43c06b9
Dario Piotrowicz <dario.piotrowicz@gmail.com>
2021-02-24 21:10:46 +0000

    set up the NavSidebar basic layout

--- src/components/NavSidebar.vue
+++ src/components/NavSidebar.vue
@@ -1,6 +1,42 @@
 <template>
   <nav class="sidebar">
-    SIDE-BAR
+    <ul class="side-nav">
+      <li class="side-nav__item">
+        <a href="#" class="side-nav__link">
+          <svg class="side-nav__icon">
+            <use xlink:href="#icon-home"></use>
+          </svg>
+          <span>Hotel</span>
+        </a>
+      </li>
+      <li class="side-nav__item">
+        <a href="#" class="side-nav__link">
+          <svg class="side-nav__icon">
+            <use xlink:href="#icon-aircraft-take-off"></use>
+          </svg>
+          <span>Flight</span>
+        </a>
+      </li>
+      <li class="side-nav__item">
+        <a href="#" class="side-nav__link">
+          <svg class="side-nav__icon">
+            <use xlink:href="#icon-key"></use>
+          </svg>
+          <span>Car rental</span>
+        </a>
+      </li>
+      <li class="side-nav__item">
+        <a href="#" class="side-nav__link">
+          <svg class="side-nav__icon">
+            <use xlink:href="#icon-map"></use>
+          </svg>
+          <span>Tours</span>
+        </a>
+      </li>
+    </ul>
+    <div class="legal">
+      &copy; 2021 by Trillo. All rights reserved.
+    </div>
   </nav>
 </template>
 
@@ -8,5 +44,23 @@
 .sidebar {
   background-color: var(--color-gray-darkest);
   flex: 0 0 18%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+}
+
+.side-nav {
+  &__icon {
+    $size: 1.75rem;
+    width: $size;
+    height: $size;
+  }
+}
+
+.legal {
+  font-size: 1.2rem;
+  text-align: center;
+  padding: 2.5rem;
+  color: var(--color-gray-medium);
 }
 </style>
