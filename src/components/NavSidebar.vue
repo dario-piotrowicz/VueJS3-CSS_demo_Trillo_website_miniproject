7d77578ab8236c5d4eed90d6954621d3d7e7015e
Dario Piotrowicz <dario.piotrowicz@gmail.com>
2021-02-25 21:53:42 +0000

    implemented the items animation for the side-nav

--- src/components/NavSidebar.vue
+++ src/components/NavSidebar.vue
@@ -54,6 +54,36 @@
   list-style: none;
   margin-top: 3.5rem;
 
+  &__item {
+    position: relative;
+
+    &:not(:last-child) {
+      margin-bottom: 0.5rem;
+    }
+
+    &::before {
+      content: "";
+      position: absolute;
+      top: 0;
+      left: 0;
+      height: 100%;
+      width: 3px;
+      background-color: var(--color-primary);
+      transform: scaleY(0);
+      transition: transform 0.2s, width 0.4s cubic-bezier(1, 0, 0, 1) 0.2s,
+        background-color 0.1s;
+    }
+
+    &:hover::before {
+      transform: scaleY(1);
+      width: 100%;
+    }
+
+    &:active::before {
+      background-color: var(--color-primary-lighter);
+    }
+  }
+
   &__link {
     &:link,
     &:visited {
@@ -64,6 +94,7 @@
       padding: 1.5rem 3rem;
       display: flex;
       align-items: center;
+      position: relative;
     }
   }
 
