739ff527ecb30db9ac43849ea3afa0ec06bc1a0f
Dario Piotrowicz <dario.piotrowicz@gmail.com>
2021-03-09 21:31:41 +0000

    set the hotel-view main-content layout up

--- src/components/HotelView/HotelView.vue
+++ src/components/HotelView/HotelView.vue
@@ -2,6 +2,14 @@
   <main class="hotel-view">
     <gallery></gallery>
     <overview></overview>
+    <div class="hotel-view__main-content">
+      <div class="hotel-description-wrapper">
+        Description
+      </div>
+      <div class="hotel-reviews-wrapper">
+        reviews
+      </div>
+    </div>
   </main>
 </template>
 
@@ -21,5 +29,21 @@ export default {
 .hotel-view {
   background-color: #fff;
   flex: 1;
+
+  &__main-content {
+    display: flex;
+    padding: 4.5rem;
+    background-color: var(--color-gray-lightest);
+    border-bottom: 1px solid var(--color-gray-lighter);
+  }
+
+  .hotel-description-wrapper {
+    flex: 0 0 60%;
+    margin-right: 4.5rem;
+  }
+
+  .hotel-reviews-wrapper {
+    flex: 1;
+  }
 }
 </style>
