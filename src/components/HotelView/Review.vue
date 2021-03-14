6d7ace08b672670e9940d6181d9a317bc99593df
Dario Piotrowicz <dario.piotrowicz@gmail.com>
2021-03-14 11:48:48 +0000

    styled the hotel review component

--- src/components/HotelView/Review.vue
+++ src/components/HotelView/Review.vue
@@ -30,5 +30,49 @@ export default {
 
 <style lang="scss" scoped>
 .hotel-review {
+  background-color: #fff;
+  box-shadow: 0 2rem 5rem transparentize(#000, 0.94);
+  padding: 3rem;
+  margin-bottom: 3.5rem;
+  font-size: 1.4rem;
+
+  &__text {
+    margin-bottom: 2rem;
+  }
+
+  &__user {
+    display: flex;
+    align-items: center;
+
+    &-details {
+      margin-right: auto;
+    }
+
+    &-picture {
+      $size: 4.5rem;
+      height: $size;
+      width: $size;
+      border-radius: 50%;
+      margin-right: 1.5rem;
+    }
+
+    &-name {
+      font-size: 1.1rem;
+      font-weight: 600;
+      text-transform: uppercase;
+      margin-bottom: 0.4rem;
+    }
+
+    &-date {
+      font-size: 1rem;
+      color: var(color-gray-dark);
+    }
+
+    &-rating {
+      color: var(--color-primary);
+      font-size: 2.2rem;
+      font-weight: 300;
+    }
+  }
 }
 </style>
